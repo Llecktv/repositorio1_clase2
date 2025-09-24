425507a9e61fa928d04ccdeaae36e64869835c7b
lleck <lleck@192.168.18.21>
2025-09-23 20:35:48 -0500

    carga inicial

---
+++ src/programas/Operaciones.java
@@ -0,0 +1,31 @@
+
+package programas;
+
+/**
+ *
+ * @author lleck
+ */
+import java.util.Scanner;
+public class Operaciones {
+    public static void main(String[]args){
+    int n1, n2, suma;
+    Scanner lectura=new Scanner(System.in);
+    //entrada 
+    System.out.print("Ingresar numero 1:");
+    n1=lectura.nextInt();
+    System.out.print("Ingresar numero 2:");
+    n2=lectura.nextInt();
+    //proceso
+    suma=n1+n2;
+    //salida
+    System.out.println("El resultado es: "+suma);
+    
+    
+    
+            
+    
+    
+    
+    
+    }
+}
